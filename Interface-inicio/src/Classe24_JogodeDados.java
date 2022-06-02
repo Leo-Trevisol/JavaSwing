b14b53ff8c6d85cc1597d8c4ccbd63a167c153c5
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2022-06-02 00:01:06 -0300

    Update Classe24_JogodeDados.java

--- Interface-inicio/src/Classe24_JogodeDados.java
+++ Interface-inicio/src/Classe24_JogodeDados.java
@@ -1,26 +1,69 @@
 
 import java.awt.Cursor;
 import java.awt.Rectangle;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
 
-public class Classe24_JogodeDados extends JFrame {
+public class Classe24_JogodeDados extends JFrame implements ActionListener {
+
+    //ADICIONANDO VARIAVEIS
 
     JFrame frame;
     JLabel titulo = null, jogada1 = null ,jogada2= null ,
             mais= null, igual= null;
-    JPanel JContentpane;
+    JPanel jContentPane;
     JButton informacao, jogardados;
+
+    //CONSTRUTOR QUE VAI RECEBER OS METODOS ===
 
     Classe24_JogodeDados(){
 
         interfeice();
-
     }
+
+
+
+    private JButton getJogardados() {
+
+        if (jogardados == null) {
+            jogardados = new JButton();
+            jogardados.setBounds(new Rectangle(253, 292, 150, 36));
+            jogardados.setText("Lançar Dados");
+            //Mensagem ao passar o mouse
+            jogardados.setToolTipText("Play");
+            //Transforma o mouse em mão
+            jogardados.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            jogardados.addActionListener(this);
+        }
+        return jogardados;
+    }
+
+    private JButton getInformacao() {
+        if (informacao == null) {
+            ImageIcon icone = new ImageIcon(getClass().getResource("/imagens/Informacao.png"));
+            informacao = new JButton(icone); // bota o icone dentro do button
+            informacao.setBounds(new Rectangle(530, 5, 40, 40));
+            informacao.setToolTipText("Informações");  //Mensagem ao passar o mouse
+            informacao.setContentAreaFilled(false);  // Tira o fundo do botão
+            informacao.setBorderPainted(false);  // Tira a borda do botão
+            informacao.setFocusable(false); // Tira o fucus do botão
+            informacao.setCursor(new Cursor(Cursor.HAND_CURSOR));  //Transforma o mouse em mão
+            informacao.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent e) {
+                            Icon imagem = new ImageIcon(getClass().getResource("/imagens/Duke.png"));
+     JOptionPane.showMessageDialog(null, "Jogo de dados em java.\n" //Mostra mensagem
+                                            + "Autor: "
+                                            + "\nClick no botão Lançar Dados para começar.",
+                                    "Jogo de Dados Java", 0, imagem);
+                        }
+                    });
+        }
+        return informacao;
+    }
+
+
 
 
 
@@ -37,52 +80,6 @@ public class Classe24_JogodeDados extends JFrame {
 
     }
 
-    /*    private JButton jogar(){
-
-            if(jogardados == null){
-
-            }
-
-        });
-
-        private JButton getInformacao(){
-            if(informacao == null){
-                ImageIcon icon = new ImageIcon(getClass().getResource("/imagens/informacao.png"));
-                informacao = new JButton(icon);
-                informacao.setBounds(new Rectangle(590,5,40,40));
-
-                // mensagem quando passar o mouse por cima
-                informacao.setToolTipText("Informaçoes");
-                // tira o fundo do botao
-                informacao.setContentAreaFilled(false);
-                // tira a borda do botao
-                informacao.setBorderPainted(false);
-                // tira o foco do botao
-                informacao.setFocusable(false);
-                // muda o cursor
-                informacao.setCursor(new Cursor(Cursor.HAND_CURSOR));
-
-
-            }
-            return informacao;
-        }
-
-        informacao.addActionListener(
-
-        new java.awt.event.ActionListener(){
-            public void ActionPerformed(java.awt.event.ActionEvent e){
-                Icon imagem = new ImageIcon(getClass().getResource("/imagens/Duke.png"));
-
-
-
-            }
-        }
-});
-
-
-
-*/
-
     private void rolardadoum(){
         int dado1 = 0;
 
@@ -94,7 +91,6 @@ public class Classe24_JogodeDados extends JFrame {
         new Classe24_JogodeDados();
 
     }
-
 
 
 }
